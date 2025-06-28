3b33620f29d5100657a5e535fed439daa9ef95da
20060602-hub <20060602@mydbs.ie>
2025-06-28 11:49:31 +0100

    Update vite.config.js

--- forever-full-stack/admin/vite.config.js
+++ forever-full-stack/admin/vite.config.js
@@ -1,8 +1,17 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {port : 5174}
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/admin/',
   plugins: [react()],
-  server: {port : 5174}
+  server: { port: 5174 }
 })
