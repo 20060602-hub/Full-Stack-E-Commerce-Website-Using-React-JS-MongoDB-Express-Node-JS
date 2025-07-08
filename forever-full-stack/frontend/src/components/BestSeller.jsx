7102910d92b0743016223b5f35d10eefb6a7645d
20060602-hub <20060602@mydbs.ie>
2025-07-08 10:16:47 +0100

    Update BestSeller.jsx

--- forever-full-stack/frontend/src/components/BestSeller.jsx
+++ forever-full-stack/frontend/src/components/BestSeller.jsx
@@ -18,7 +18,8 @@ const BestSeller = () => {
       <div className='text-center text-3xl py-8'>
         <Title text1={'BEST'} text2={'SELLERS'}/>
         <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the.
+       Timeless fashion that tells your story.  
+Crafted for the bold. Worn with grace.
         </p>
       </div>
 
